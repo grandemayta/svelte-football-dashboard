2564f4f42043ced395492a9ef5268f4cb0500589
grandemayta <grandemayta@gmail.com>
2021-03-17 09:20:41 +0100

    improve dashboard

--- src/App.svelte
+++ src/App.svelte
@@ -2,21 +2,11 @@
   import Header from "./components/Header.svelte";
   import Footer from "./components/Footer.svelte";
   import FootballSection from "./components/FootballSection.svelte";
-  import Lazy from "./components/Lazy.svelte";
 </script>
 
 <Header />
 
 <FootballSection competitionId={2019} competitionCode="SA" theme="is-link" />
-<Lazy height={800}>
-  <FootballSection competitionId={2014} competitionCode="PD" theme="is-success" />
-</Lazy>
-<Lazy height={800}>
-  <FootballSection competitionId={2021} competitionCode="PL" theme="is-info" />
-</Lazy>
-<Lazy height={800}>
-  <FootballSection competitionId={2002} competitionCode="BL1" theme="is-danger" />
-</Lazy>
 
 
 <Footer author="Gabriel Mayta" />
