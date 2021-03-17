b876de0d497cc429ada1a1e7d59aea21475b49a3
grandemayta <grandemayta@gmail.com>
2021-03-17 09:44:51 +0100

    add bulma cdn

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,18 @@
   import Footer from "./components/Footer.svelte";
   import FootballSection from "./components/FootballSection.svelte";
 </script>
+<style>
+  :global(body) {
+		overflow: hidden;
+	}
+</style>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+</svelte:head>
 
 <Header />
 
 <FootballSection competitionId={2019} competitionCode="SA" theme="is-link" />
-
 
 <Footer author="Gabriel Mayta" />
