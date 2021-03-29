2f61eae195746464d45a5be8006e3a46b5e66cd2
grandemayta <grandemayta@gmail.com>
2021-03-29 15:08:33 +0200

    add error component

---
+++ src/components/Error.svelte
@@ -0,0 +1,9 @@
+<div class="container mt-5 mb-5">
+  <div class="card">
+    <div class="card-content">
+      <div class="content">
+        <p>We reached the limit usage <strong>(10req/min)</strong></p>
+      </div>
+    </div>
+  </div>  
+</div>
